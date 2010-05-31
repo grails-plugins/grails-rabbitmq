f8ddcf1db53750c539699e858a03ec211b96038f
Jeff Brown <jeff@jeffandbetsy.net>
2010-05-31 16:57:07 -0500

    upgrade to Grails 1.3.1

--- RabbitmqGrailsPlugin.groovy
+++ RabbitmqGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class RabbitmqGrailsPlugin {
     // the plugin version
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.0.RC2 > *"
+    def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
