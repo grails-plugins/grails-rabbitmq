6a795a67d8600a9aed420197a6050207d8f68237
Jeff Brown <jeff@jeffandbetsy.net>
2010-08-07 10:03:59 -0500

    improve the queue builder

--- src/groovy/org/grails/rabbitmq/RabbitQueueBuilder.groovy
+++ src/groovy/org/grails/rabbitmq/RabbitQueueBuilder.groovy
@@ -8,6 +8,16 @@ class RabbitQueueBuilder {
     
     def methodMissing(String methodName, args) {
         def queue = new Queue(methodName)
+
+        if(args) {
+            def argsMap = args[0]
+            queue.autoDelete = Boolean.valueOf(argsMap.autoDelete)
+            queue.exclusive = Boolean.valueOf(argsMap.exclusive)
+            queue.durable = Boolean.valueOf(argsMap.durable)
+            if(argsMap.arguments instanceof Map) {
+                queue.arguments = argsMap.arguments
+            }
+        }
         queues << queue
     }
 }
--- test/unit/org/grails/rabbitmq/RabbitQueueBuilderTests.groovy
+++ test/unit/org/grails/rabbitmq/RabbitQueueBuilderTests.groovy
@@ -22,4 +22,45 @@ class RabbitQueueBuilderTests extends GroovyTestCase {
         assertEquals 'wrong exclusive value', false, queue.exclusive
         assertNull 'no arguments expected', queue.arguments
     }
+
+    void testQueueConfigurationDetails() {
+        def closure = {
+            foo autoDelete: true, durable: false, exclusive: true
+            bar autoDelete: false, durable: true, exclusive: false
+            baz autoDelete: true, arguments: [lang: 'Groovy', framework: 'Grails']
+        }
+
+        def builder = new RabbitQueueBuilder()
+
+        closure.delegate = builder
+        closure()
+
+        def queues = builder.queues
+        assertEquals 'wrong number of queues', 3, queues?.size()
+        def queue = queues[0]
+
+        assertEquals 'wrong queue name', 'foo', queue.name
+        assertEquals 'wrong durable value', false, queue.durable
+        assertEquals 'wrong auto delete value', true, queue.autoDelete
+        assertEquals 'wrong exclusive value', true, queue.exclusive
+        assertNull 'no arguments expected', queue.arguments
+
+        queue = queues[1]
+
+        assertEquals 'wrong queue name', 'bar', queue.name
+        assertEquals 'wrong durable value', true, queue.durable
+        assertEquals 'wrong auto delete value', false, queue.autoDelete
+        assertEquals 'wrong exclusive value', false, queue.exclusive
+        assertNull 'no arguments expected', queue.arguments
+
+        queue = queues[2]
+
+        assertEquals 'wrong queue name', 'baz', queue.name
+        assertEquals 'wrong durable value', false, queue.durable
+        assertEquals 'wrong auto delete value', true, queue.autoDelete
+        assertEquals 'wrong exclusive value', false, queue.exclusive
+        assertEquals 'wrong number of arguments', 2, queue.arguments?.size()
+        assertEquals 'lang argument had wrong value', 'Groovy', queue.arguments.lang
+        assertEquals 'framework argument had wrong value', 'Grails', queue.arguments.framework
+    }
 }
