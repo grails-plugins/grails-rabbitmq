78e25dfc01ce85ae107660db48eead37f9cda48b
Jeff Brown <jbrown@vmware.com>
2012-11-28 09:14:30 -0600

    version 1.0.0

--- RabbitmqGrailsPlugin.groovy
+++ RabbitmqGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import org.springframework.retry.support.RetryTemplate
 
 class RabbitmqGrailsPlugin {
     // the plugin version
-    def version = "1.0.0.BUILD-SNAPSHOT"
+    def version = "1.0.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
