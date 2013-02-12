f8f164c2f41a94e99403bbd1e896fb32ee1e4c72
Bud Byrd <bud.byrd@rackspace.com>
2013-02-12 11:04:47 -0600

    Version bump

--- RabbitmqRaxGrailsPlugin.groovy
+++ RabbitmqRaxGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import org.springframework.retry.support.RetryTemplate
 
 class RabbitmqRaxGrailsPlugin {
     // the plugin version
-    def version = "1.0.3"
+    def version = "1.0.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
