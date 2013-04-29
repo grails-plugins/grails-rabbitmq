01f84ca3dd08a22012c2f2ecf54a1486d1ef7a94
Bud Byrd <bud.byrd@rackspace.com>
2013-04-29 13:40:47 -0500

    Version bump

--- RabbitmqRaxGrailsPlugin.groovy
+++ RabbitmqRaxGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import org.springframework.retry.support.RetryTemplate
 
 class RabbitmqRaxGrailsPlugin {
     // the plugin version
-    def version = "1.0.9"
+    def version = "1.0.10"
     // plugin group
     def group = 'com.rackspace.rvi'
     // the version or versions of Grails the plugin is designed for
