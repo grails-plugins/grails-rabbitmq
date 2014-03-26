e85d54ff3d622c801a7c353c7b4da93025aafe45
Daniel Bower <daniel.d.bower@gmail.com>
2014-03-26 13:40:42 -0400

    Move work directory to target

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,4 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
+grails.project.work.dir = 'target'
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
