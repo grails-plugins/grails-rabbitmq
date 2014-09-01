284e301d93d0fa6744339ff0fcf8d50e28d49eb3
yellowsnow <contact@arkilog.ma>
2014-09-01 23:40:04 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grails clean && grails $UPGRADE --non-interactive && grails test-app unit: --non-interactive --stacktrace
+grails $UPGRADE --non-interactive && grails test-app unit: --non-interactive --stacktrace
