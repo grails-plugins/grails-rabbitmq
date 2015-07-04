c912da002d9210010969613ec2f0c8a6484e756c
Puneet Behl <puneet.behl@intelligrape.com>
2015-07-04 18:14:00 +0530

    Commented publish steps from travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -4,18 +4,18 @@ rm -rf *.zip
 ./gradlew clean test jar
 
 
-EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+#EXIT_STATUS=0
+#echo "Publishing archives for branch $TRAVIS_BRANCH"
+#if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+#
+#  echo "Publishing archives"
+#
+#  if [[ -n $TRAVIS_TAG ]]; then
+#      ./gradlew bintrayUpload || EXIT_STATUS=$?
+#  else
+#      ./gradlew publish || EXIT_STATUS=$?
+#  fi
+#
+#fi
 
-  echo "Publishing archives"
-
-  if [[ -n $TRAVIS_TAG ]]; then
-      ./gradlew bintrayUpload || EXIT_STATUS=$?
-  else
-      ./gradlew publish || EXIT_STATUS=$?
-  fi
-
-fi
-
-exit $EXIT_STATUS
+#exit $EXIT_STATUS
